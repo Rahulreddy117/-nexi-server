910ed5528600de7fe8c0481013d8151da4dfc68b
Rahulreddy117 <switon89@gmail.com>
2025-10-31 21:12:05 +0530

    Updated again config.ts

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 export const config = {
   databaseURI: "mongodb+srv://admin:Reddy_117@nexi-cluster.l9phoa7.mongodb.net/?appName=nexi-cluster",
-  appId: "nexi-social-app",
-  masterKey: "x7K!pL9#vM2$qN8&wE4@rT6",
+  appId: "myAppId",
+  masterKey: "myMasterKey",
   serverURL: "https://nexi-server.onrender.com/parse",
   cloud: "./cloud/main.js",
   allowClientClassCreation: true,
